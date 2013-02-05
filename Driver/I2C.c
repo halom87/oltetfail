e035783b174a0f0db4d810ef741e1aeec3feab62
kundralaci <laszlo.kundra@gmail.com>
2013-02-05 15:41:45 +0100

    átmeneti iteráció-fix

--- Driver/I2C.c
+++ Driver/I2C.c
@@ -42,25 +42,73 @@ void I2C_Config(void)
 
 uint8_t I2C_BufferRead(uint8_t* Data, uint8_t deviceAddress, uint8_t ReadAddr, uint8_t len)
 {
+	int i=0;
+	if (xSemaphoreTake(I2CBusy,portMAX_DELAY))
+	{
+		for (i=0; i<len; i++)
+		{
+			while (I2C_GetFlagStatus(I2C2,I2C_FLAG_BUSY));
+
+			I2C_ClearFlag(I2C2,I2C_FLAG_AF);
+			I2C_AcknowledgeConfig(I2C2, ENABLE);
+
+			I2C_GenerateSTART(I2C2, ENABLE);
+			while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT));
+
+			I2C_Send7bitAddress(I2C2, deviceAddress, I2C_Direction_Transmitter);
+			while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
+			if (len>1) ReadAddr|=0x80;
+			I2C_SendData(I2C2, ReadAddr);
+			while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
+			I2C_GenerateSTART(I2C2,ENABLE);
+			while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT));
+
+			I2C_Send7bitAddress(I2C2, deviceAddress, I2C_Direction_Receiver);
+			while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+
+			I2C_AcknowledgeConfig(I2C2,DISABLE);
+			while (!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_BYTE_RECEIVED));
+
+			*Data=I2C_ReceiveData(I2C2);
+
+			I2C_GenerateSTOP(I2C2, ENABLE);
+			while(I2C_GetFlagStatus(I2C2, I2C_FLAG_STOPF)); // stop bit flag
+		}
+
+	 	xSemaphoreGive(I2CBusy);
+	    return 1;
+	}
+	return 0;
+
+	/*
 	if (xSemaphoreTake(I2CBusy,portMAX_DELAY))
 	{
 	    while (I2C_GetFlagStatus(I2C2,I2C_FLAG_BUSY));
+
 	    I2C_ClearFlag(I2C2,I2C_FLAG_AF);
 	    I2C_AcknowledgeConfig(I2C2, ENABLE);
-		I2C_GenerateSTART(I2C2, ENABLE);
 
+		I2C_GenerateSTART(I2C2, ENABLE);
 		while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT));
+
 		I2C_Send7bitAddress(I2C2, deviceAddress, I2C_Direction_Transmitter);
 	    while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
 	    if (len>1) ReadAddr|=0x80;
 	    I2C_SendData(I2C2, ReadAddr);
 	    while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
 	    I2C_GenerateSTART(I2C2,ENABLE);
 		while(!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_MODE_SELECT));
+
 	    I2C_Send7bitAddress(I2C2, deviceAddress, I2C_Direction_Receiver);
 	    while (!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_RECEIVER_MODE_SELECTED));
+
 		I2C_AcknowledgeConfig(I2C2,DISABLE);
 	    while (!I2C_CheckEvent(I2C2,I2C_EVENT_MASTER_BYTE_RECEIVED));
+
 		*Data=I2C_ReceiveData(I2C2);
 
 		//DMA-s olvasás, jó lenne, ha menne
@@ -76,14 +124,15 @@ uint8_t I2C_BufferRead(uint8_t* Data, uint8_t deviceAddress, uint8_t ReadAddr, u
 //	    I2C_DMACmd(I2C2,DISABLE);
 //	    DMA_Cmd(DMA1_Channel5, DISABLE);
 //      DMA_ClearFlag(DMA1_FLAG_TC5);
+
 	    I2C_GenerateSTOP(I2C2, ENABLE);
 	 	while(I2C_GetFlagStatus(I2C2, I2C_FLAG_STOPF)); // stop bit flag
+
 	 	xSemaphoreGive(I2CBusy);
 	    return 1;
-
-
 	}
 	return 0;
+	*/
 }
 uint8_t I2C_ByteWrite(uint8_t Data, uint8_t deviceAddress, uint8_t WriteAddr)
 {
@@ -95,21 +144,21 @@ uint8_t I2C_ByteWrite(uint8_t Data, uint8_t deviceAddress, uint8_t WriteAddr)
 
 		I2C_GenerateSTART(I2C2, ENABLE);
 	    while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_MODE_SELECT));
+
 	    I2C_Send7bitAddress(I2C2, deviceAddress, I2C_Direction_Transmitter);
 	    while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_TRANSMITTER_MODE_SELECTED));
+
 	    I2C_SendData(I2C2, WriteAddr);
 	    while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
 	    I2C_SendData(I2C2, Data);
 	    while(!I2C_CheckEvent(I2C2, I2C_EVENT_MASTER_BYTE_TRANSMITTED));
+
 	    I2C_GenerateSTOP(I2C2, ENABLE);
 	    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_STOPF));
 
 		xSemaphoreGive(I2CBusy);
 	    return 1;
-
-
-
 	}
-
 	return 0;
 }
